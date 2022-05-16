3870c49ff18f3fd2abb8afff99ee8e208c4e8976
Eldes <github@eldes.com>
2022-05-16 19:11:20 -0300

    Consegue a lista de itens do back-end

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,147 +1,10 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { FlatList, ListRenderItem, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import FloatingActionButton from '../components/FloatingActionButton';
 import Item from '../models/item';
 import { StackParams } from './navigator';
-
-const data: Item[] = [
-  {
-    id: 1,
-    nome: 'Coca-cola',
-    descricao: 'Refrigerante em lata.',
-  },
-  {
-    id: 2,
-    nome: 'Pudim',
-    descricao: 'Pudim de leite condensado.',
-  },
-  {
-    id: 3,
-    nome: 'Cachorro-quente',
-    descricao: 'Lanche grande.',
-  },
-  {
-    id: 4,
-    nome: 'Coca-cola',
-    descricao: 'Refrigerante em lata.',
-  },
-  {
-    id: 5,
-    nome: 'Pudim',
-    descricao: 'Pudim de leite condensado.',
-  },
-  {
-    id: 6,
-    nome: 'Cachorro-quente',
-    descricao: 'Lanche grande.',
-  },
-  {
-    id: 7,
-    nome: 'Coca-cola',
-    descricao: 'Refrigerante em lata.',
-  },
-  {
-    id: 8,
-    nome: 'Pudim',
-    descricao: 'Pudim de leite condensado.',
-  },
-  {
-    id: 9,
-    nome: 'Cachorro-quente',
-    descricao: 'Lanche grande.',
-  },
-  {
-    id: 10,
-    nome: 'Coca-cola',
-    descricao: 'Refrigerante em lata.',
-  },
-  {
-    id: 11,
-    nome: 'Pudim',
-    descricao: 'Pudim de leite condensado.',
-  },
-  {
-    id: 12,
-    nome: 'Cachorro-quente',
-    descricao: 'Lanche grande.',
-  },
-  {
-    id: 13,
-    nome: 'Coca-cola',
-    descricao: 'Refrigerante em lata.',
-  },
-  {
-    id: 14,
-    nome: 'Pudim',
-    descricao: 'Pudim de leite condensado.',
-  },
-  {
-    id: 15,
-    nome: 'Cachorro-quente',
-    descricao: 'Lanche grande.',
-  },
-  {
-    id: 16,
-    nome: 'Coca-cola',
-    descricao: 'Refrigerante em lata.',
-  },
-  {
-    id: 17,
-    nome: 'Pudim',
-    descricao: 'Pudim de leite condensado.',
-  },
-  {
-    id: 18,
-    nome: 'Cachorro-quente',
-    descricao: 'Lanche grande.',
-  },
-  {
-    id: 19,
-    nome: 'Coca-cola',
-    descricao: 'Refrigerante em lata.',
-  },
-  {
-    id: 20,
-    nome: 'Pudim',
-    descricao: 'Pudim de leite condensado.',
-  },
-  {
-    id: 21,
-    nome: 'Cachorro-quente',
-    descricao: 'Lanche grande.',
-  },
-  {
-    id: 22,
-    nome: 'Coca-cola',
-    descricao: 'Refrigerante em lata.',
-  },
-  {
-    id: 23,
-    nome: 'Pudim',
-    descricao: 'Pudim de leite condensado.',
-  },
-  {
-    id: 24,
-    nome: 'Cachorro-quente',
-    descricao: 'Lanche grande.',
-  },
-  {
-    id: 25,
-    nome: 'Coca-cola',
-    descricao: 'Refrigerante em lata.',
-  },
-  {
-    id: 26,
-    nome: 'Pudim',
-    descricao: 'Pudim de leite condensado.',
-  },
-  {
-    id: 27,
-    nome: 'Cachorro-quente',
-    descricao: 'Lanche grande.',
-  },
-]
 
 const styles = StyleSheet.create({
   container: {
@@ -187,6 +50,18 @@ const styles = StyleSheet.create({
 type Props = NativeStackScreenProps<StackParams, 'Home'>;
 
 const HomeScreen: React.FC<Props> = (props) => {
+
+  const [data, setData] = useState<Item[]>([]);
+
+  useEffect(() => {
+    axios.get<Item[]>('http://localhost:4000/api/itens')
+    .then((res) => {
+      setData(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  },[data]);
 
   const renderItem: ListRenderItem<Item> = ({item}) => {
     
