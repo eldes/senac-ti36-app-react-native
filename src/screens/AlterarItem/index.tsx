a2bf30002cb09e5a4549282ca6b60bd8ccb9abd0
Eldes <github@eldes.com>
2022-05-16 21:40:38 -0300

    Conecta a tela AlterarItem com o back-end

--- src/screens/AlterarItem/index.tsx
+++ src/screens/AlterarItem/index.tsx
@@ -1,28 +1,39 @@
 import { StackActions } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import axios from 'axios';
 import React, { useState } from 'react';
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { View } from 'react-native';
 import ItemForm from '../../components/ItemForm';
+import Loading from '../../components/Loading';
 import Item from '../../models/item';
-import ItemScreen from '../Item';
 import { StackParams } from '../navigator';
-import styles from './styles';
 
 type Props = NativeStackScreenProps<StackParams, 'AlterarItem'>;
 
 const AlterarItemScreen: React.FC<Props> = (props) => {
+	const [loading, setLoading] = useState(false);
 	const item = props.route.params.item;
 
-	const [nome, setNome] = useState(item.nome);
-	const [descricao, setDescricao] = useState(item.descricao);
-
 	const itemFormSalvar = (item: Item) => {
-		props.navigation.pop(1);
-		props.navigation.dispatch(StackActions.replace('Item', {item}));
+		setLoading(true);
+		axios.put(`http://localhost:4000/api/itens/${item.id}`, item)
+		.then(() => {
+			props.navigation.pop(1);
+			props.navigation.dispatch(StackActions.replace('Item', {item}));
+			setLoading(false);
+		})
+		.catch((error) => {
+			alert(error.message);
+			setLoading(false);
+		});
+		
 	};
 
 	return (
-		<ItemForm item={item} onSalvar={itemFormSalvar} />
+		<View>
+			<ItemForm item={item} onSalvar={itemFormSalvar} />
+			<Loading show={loading} />
+		</View>
 	);
 };
 
