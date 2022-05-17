e8d3c1853a55903689333fbbdb226d437b104e4e
Eldes <github@eldes.com>
2022-05-16 21:56:02 -0300

    Remove um item

--- src/screens/Item/index.tsx
+++ src/screens/Item/index.tsx
@@ -1,6 +1,9 @@
+import { StackActions } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import Loading from '../../components/Loading';
 import { StackParams } from '../navigator';
 import styles from './styles';
 
@@ -9,12 +12,29 @@ type Props = NativeStackScreenProps<StackParams, 'Item'>;
 const ItemScreen: React.FC<Props> = (props) => {
 	const item = props.route.params.item;
 
+	const [loading, setLoading] = useState(false);
+
 	useEffect(() => {
 		props.navigation.setOptions({ title: item.nome })
 	}, []);
 
 	const botaoEditarPressionado = () => {
 		props.navigation.navigate('AlterarItem', {item: item});
+	};
+
+	const botaoRemoverPressionado = () => {
+		setLoading(true);
+		axios.delete(`http://localhost:4000/api/itens/${item.id}`)
+		.then(() => {
+			setLoading(false);
+			props.navigation.pop(1);
+			props.navigation.dispatch(StackActions.replace('Home'));
+		})
+		.catch(error => {
+			alert(error.message);
+			setLoading(false);
+		});
+		
 	};
 
 	return (
@@ -26,7 +46,11 @@ const ItemScreen: React.FC<Props> = (props) => {
 				<TouchableOpacity style={styles.botao}>
 					<Text style={styles.botaoTexto} onPress={botaoEditarPressionado}>Editar</Text>
 				</TouchableOpacity>
+				<TouchableOpacity style={styles.botao}>
+					<Text style={styles.botaoTexto} onPress={botaoRemoverPressionado}>Remover</Text>
+				</TouchableOpacity>
 			</View>
+			<Loading show={loading} />
 		</View>
 	);
 };
